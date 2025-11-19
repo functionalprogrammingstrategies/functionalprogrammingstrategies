JonasAckermann/scala-with-cats

Working Typst build
- Markdown transformed to Typst. There are still some needed changes but
this is sufficient to get the source building.

- Rework the build system to use Typst and remove Pandoc

src/pages/applicatives/parallel.typ
@@ -1,0 +1,172 @@
+#import "../stdlib.typ": info, warning, solution
+== Parallel
+
+
+In the previous section we saw that
+when call `product` on a type that
+has a `Monad` instance
+we get sequential semantics.
+This makes sense from the point-of-view
+of keeping consistency with 
+implementations of `product` in terms of `flatMap` and `map`.
+However it's not always what we want.
+The `Parallel` type class, and its associated syntax,
+allows us to access alternate semantics
+for certain monads.
+
+We've seen how the `product` method on `Either`
+stops at the first error.
+
+```scala mdoc:silent
+import cats.Semigroupal
+import cats.instances.either._ // for Semigroupal
+
+type ErrorOr[A] = Either[Vector[String], A]
+val error1: ErrorOr[Int] = Left(Vector("Error 1"))
+val error2: ErrorOr[Int] = Left(Vector("Error 2"))
+```
+
+```scala mdoc
+Semigroupal[ErrorOr].product(error1, error2)
+```
+
+We can also write this
+using `tupled`
+as a short-cut.
+
+```scala mdoc:silent
+import cats.syntax.apply._ // for tupled
+import cats.instances.vector._ // for Semigroup on Vector
+```
+```scala mdoc
+(error1, error2).tupled
+```
+
+To collect all the errors
+we simply replace `tupled` with its "parallel" version
+called `parTupled`.
+
+```scala mdoc:silent
+import cats.syntax.parallel._ // for parTupled
+```
+```scala mdoc
+(error1, error2).parTupled
+```
+
+Notice that both errors are returned! 
+This behaviour is not special to using `Vector` as the error type.
+Any type that has a `Semigroup` instance will work.
+For example, here we use `List` instead.
+
+```scala mdoc:silent
+import cats.instances.list._ // for Semigroup on List
+
+type ErrorOrList[A] = Either[List[String], A]
+val errStr1: ErrorOrList[Int] = Left(List("error 1"))
+val errStr2: ErrorOrList[Int] = Left(List("error 2"))
+```
+```scala mdoc
+
+(errStr1, errStr2).parTupled
+```
+
+There are many syntax methods provided by `Parallel`
+for methods on `Semigroupal` and related types,
+but the most commonly used is `parMapN`.
+Here's an example of `parMapN` 
+in an error handling situation.
+
+```scala mdoc:silent
+val success1: ErrorOr[Int] = Right(1)
+val success2: ErrorOr[Int] = Right(2)
+val addTwo = (x: Int, y: Int) => x + y
+```
+```scala mdoc
+(error1, error2).parMapN(addTwo)
+(success1, success2).parMapN(addTwo)
+```
+
+Let's dig into how `Parallel` works.
+The definition below is the core of `Parallel`.
+
+```scala
+trait Parallel[M[_]] {
+  type F[_]
+  
+  def applicative: Applicative[F]
+  def monad: Monad[M]
+  def parallel: ~>[M, F]
+}
+```
+
+This tells us if there is a `Parallel` instance for some type constructor `M` then:
+
+- there must be a `Monad` instance for `M`;
+- there is a related type constructor `F` that has an `Applicative` instance; and
+- we can convert `M` to `F`.
+
+We haven't seen `~>` before. 
+It's a type alias for [`FunctionK`][cats.arrow.FunctionK] 
+and is what performs the conversion from `M` to `F`. 
+A normal function `A => B` converts values of type `A` to values of type `B`. 
+Remember that `M` and `F` are not types; they are type constructors. 
+A `FunctionK` `M ~> F` is a function from a value with type `M[A]` to a value with type `F[A]`. 
+Let's see a quick example 
+by defining a `FunctionK` that converts an `Option` to a `List`.
+
+```scala mdoc:silent
+import cats.arrow.FunctionK
+
+object optionToList extends FunctionK[Option, List] {
+  def apply[A](fa: Option[A]): List[A] =
+    fa match {
+      case None    => List.empty[A]
+      case Some(a) => List(a)
+    }
+}
+```
+```scala mdoc
+optionToList(Some(1))
+optionToList(None)
+```
+
+As the type parameter `A` is generic a `FunctionK` cannot inspect
+any values contained with the type constructor `M`.
+The conversion must be performed
+purely in terms of the structure of the type constructors `M` and `F`.
+We can see in `optionToList` above
+this is indeed the case.
+
+So in summary,
+`Parallel` allows us to take a type that has a monad instance
+and convert it to some related type 
+that instead has an applicative (or semigroupal) instance.
+This related type will have some useful alternate semantics.
+We've seen the case above where the related applicative for `Either`
+allows for accumulation of errors
+instead of fail-fast semantics.
+
+Now we've seen `Parallel`
+it's time to finally learn about `Applicative`.
+
+
+==== Exercise: Parallel List
+
+
+Does `List` have a `Parallel` instance? If so, what does the `Parallel` instance do?
+
+#solution[
+`List` does have a `Parallel` instance, 
+and it zips the `List`
+insted of creating the cartesian product.
+
+We can see by writing a little bit of code.
+
+```scala mdoc:silent
+import cats.instances.list._
+```
+```scala mdoc
+(List(1, 2), List(3, 4)).tupled
+(List(1, 2), List(3, 4)).parTupled
+```
+]

src/pages/codata/codata.typ
@@ -1,0 +1,63 @@
+#import "../stdlib.typ": info, warning, solution
+== Data and Codata
+
+
+Data describes what things are, while codata describes what things can do. 
+
+We have seen that data is defined in terms of constructors producing elements of the data type. Let's take a very simple example: a `Bool` is either `True` or `False`. We know we can represent this in Scala as
+
+```scala mdoc:silent
+enum Bool {
+  case True
+  case False
+}
+```
+
+The definition tells us there are two ways to construct an element of type `Bool`.
+Furthermore, if we have such an element we can tell exactly which case it is, by using a pattern match for example. Similarly, if the instances themselves hold data, as in `List` for example, we can always extract all the data within them. Again, we can use pattern matching to achieve this.
+
+Codata, in contrast, is defined in terms of operations we can perform on the elements of the type. These operations are sometimes called *destructors* (which we've already encountered), *observations*, or *eliminators*. A common example of codata is a data structures such as a set. We might define the operations on a `Set` with elements of type `A` as:
+
+- `contains` which takes a `Set[A]` and an element `A` and returns a `Boolean` indicating if the set contains the element;
+- `insert` which takes a `Set[A]` and an element `A` and returns a `Set[A]` containing all the elements from the original set and the new element; and
+- `union` which takes a `Set[A]` and a set `Set[A]` and returns a `Set[A]` containing all the elements of both sets.
+
+In Scala we could implement this definition as
+
+```scala mdoc:silent
+trait Set[A] {
+  
+  /** True if this set contains the given element */
+  def contains(elt: A): Boolean
+  
+  /** Construct a new set containing all elements in this set and the given element */
+  def insert(elt: A): Set[A]
+  
+  /** Construct the union of this and that set */
+  def union(that: Set[A]): Set[A]
+}
+```
+
+This definition does not tell us anything about the internal representation of the elements in the set. It could use a hash table, a tree, or something more exotic. It does, however, tell us what we can do with the set. We know we can take the union but not the intersection, for example. 
+
+If you come from the object-oriented world you might recognize the description of codata above as programming to an interface. In some ways codata is just taking concepts from the object-oriented world and presenting them in a way that is consistent with the rest of the functional programming paradigm. However, this does not mean adopting all the features of object-oriented programming. We won't use state, which is difficult to reason about. We also won't use implementation inheritance either, for the same reason. In our subset of object-oriented programming we'll either be defining interfaces (which may have default implementations of some methods) or final classes that implement those interfaces. Interestingly, this subset of object-oriented programming is often recommended by advocates of object-oriented programming.
+
+Let's now be a little more precise in our definition of codata, which will make the duality between data and codata clearer. Remember the definition of data: it is defined in terms of sums (logical ors) and products (logical ands). We can transform any data into a sum of products. Each product in the sum is a constructor, and the product itself is the parameters that the constructor accepts. Finally, we can think of constructors as functions which take some arbitrary input and produce an element of data. Our end point is a sum of functions from arbitrary input to data.
+
+More abstractly, if we are constructing an element of some data type `A` we call one of the constructors
+
+- `A1: (B, C, ...) => A`; or
+- `A2: (D, E, ...) => A`; or
+- `A3: (F, G, ...) => A`; and so on.
+
+
+Now we'll turn to codata. Codata is defined as a product of functions, these functions being the destructors. The input to a destructor is always an element of the codata type and possibly some other parameters. The output is usually something that is not of the codata type. Thus constructing an element of some codata type `A` means defining
+
+
+- `A1: (A, B, ...) => C`; and
+- `A2: (A, D, ...) => E`; and
+- `A3: (A, F, ...) => G`; and so on.
+
+This hopefully makes the duality between the two clearer.
+
+Now we understand what codata is, we will turn to representing codata in Scala.

functionalprogrammingstrategies/functionalprogrammingstrategies

src/pages/applicatives/parallel.typ
@@ -1,0 +1,172 @@
+#import "../stdlib.typ": info, warning, solution
+== Parallel
+
+
+In the previous section we saw that
+when call `product` on a type that
+has a `Monad` instance
+we get sequential semantics.
+This makes sense from the point-of-view
+of keeping consistency with 
+implementations of `product` in terms of `flatMap` and `map`.
+However it's not always what we want.
+The `Parallel` type class, and its associated syntax,
+allows us to access alternate semantics
+for certain monads.
+
+We've seen how the `product` method on `Either`
+stops at the first error.
+
+```scala mdoc:silent
+import cats.Semigroupal
+import cats.instances.either._ // for Semigroupal
+
+type ErrorOr[A] = Either[Vector[String], A]
+val error1: ErrorOr[Int] = Left(Vector("Error 1"))
+val error2: ErrorOr[Int] = Left(Vector("Error 2"))
+```
+
+```scala mdoc
+Semigroupal[ErrorOr].product(error1, error2)
+```
+
+We can also write this
+using `tupled`
+as a short-cut.
+
+```scala mdoc:silent
+import cats.syntax.apply._ // for tupled
+import cats.instances.vector._ // for Semigroup on Vector
+```
+```scala mdoc
+(error1, error2).tupled
+```
+
+To collect all the errors
+we simply replace `tupled` with its "parallel" version
+called `parTupled`.
+
+```scala mdoc:silent
+import cats.syntax.parallel._ // for parTupled
+```
+```scala mdoc
+(error1, error2).parTupled
+```
+
+Notice that both errors are returned! 
+This behaviour is not special to using `Vector` as the error type.
+Any type that has a `Semigroup` instance will work.
+For example, here we use `List` instead.
+
+```scala mdoc:silent
+import cats.instances.list._ // for Semigroup on List
+
+type ErrorOrList[A] = Either[List[String], A]
+val errStr1: ErrorOrList[Int] = Left(List("error 1"))
+val errStr2: ErrorOrList[Int] = Left(List("error 2"))
+```
+```scala mdoc
+
+(errStr1, errStr2).parTupled
+```
+
+There are many syntax methods provided by `Parallel`
+for methods on `Semigroupal` and related types,
+but the most commonly used is `parMapN`.
+Here's an example of `parMapN` 
+in an error handling situation.
+
+```scala mdoc:silent
+val success1: ErrorOr[Int] = Right(1)
+val success2: ErrorOr[Int] = Right(2)
+val addTwo = (x: Int, y: Int) => x + y
+```
+```scala mdoc
+(error1, error2).parMapN(addTwo)
+(success1, success2).parMapN(addTwo)
+```
+
+Let's dig into how `Parallel` works.
+The definition below is the core of `Parallel`.
+
+```scala
+trait Parallel[M[_]] {
+  type F[_]
+  
+  def applicative: Applicative[F]
+  def monad: Monad[M]
+  def parallel: ~>[M, F]
+}
+```
+
+This tells us if there is a `Parallel` instance for some type constructor `M` then:
+
+- there must be a `Monad` instance for `M`;
+- there is a related type constructor `F` that has an `Applicative` instance; and
+- we can convert `M` to `F`.
+
+We haven't seen `~>` before. 
+It's a type alias for [`FunctionK`][cats.arrow.FunctionK] 
+and is what performs the conversion from `M` to `F`. 
+A normal function `A => B` converts values of type `A` to values of type `B`. 
+Remember that `M` and `F` are not types; they are type constructors. 
+A `FunctionK` `M ~> F` is a function from a value with type `M[A]` to a value with type `F[A]`. 
+Let's see a quick example 
+by defining a `FunctionK` that converts an `Option` to a `List`.
+
+```scala mdoc:silent
+import cats.arrow.FunctionK
+
+object optionToList extends FunctionK[Option, List] {
+  def apply[A](fa: Option[A]): List[A] =
+    fa match {
+      case None    => List.empty[A]
+      case Some(a) => List(a)
+    }
+}
+```
+```scala mdoc
+optionToList(Some(1))
+optionToList(None)
+```
+
+As the type parameter `A` is generic a `FunctionK` cannot inspect
+any values contained with the type constructor `M`.
+The conversion must be performed
+purely in terms of the structure of the type constructors `M` and `F`.
+We can see in `optionToList` above
+this is indeed the case.
+
+So in summary,
+`Parallel` allows us to take a type that has a monad instance
+and convert it to some related type 
+that instead has an applicative (or semigroupal) instance.
+This related type will have some useful alternate semantics.
+We've seen the case above where the related applicative for `Either`
+allows for accumulation of errors
+instead of fail-fast semantics.
+
+Now we've seen `Parallel`
+it's time to finally learn about `Applicative`.
+
+
+==== Exercise: Parallel List
+
+
+Does `List` have a `Parallel` instance? If so, what does the `Parallel` instance do?
+
+#solution[
+`List` does have a `Parallel` instance, 
+and it zips the `List`
+instead of creating the cartesian product.
+
+We can see by writing a little bit of code.
+
+```scala mdoc:silent
+import cats.instances.list._
+```
+```scala mdoc
+(List(1, 2), List(3, 4)).tupled
+(List(1, 2), List(3, 4)).parTupled
+```
+]

src/pages/codata/codata.typ
@@ -1,0 +1,63 @@
+#import "../stdlib.typ": info, warning, solution
+== Data and Codata
+
+
+Data describes what things are, while codata describes what things can do. 
+
+We have seen that data is defined in terms of constructors producing elements of the data type. Let's take a very simple example: a `Bool` is either `True` or `False`. We know we can represent this in Scala as
+
+```scala mdoc:silent
+enum Bool {
+  case True
+  case False
+}
+```
+
+The definition tells us there are two ways to construct an element of type `Bool`.
+Furthermore, if we have such an element we can tell exactly which case it is, by using a pattern match for example. Similarly, if the instances themselves hold data, as in `List` for example, we can always extract all the data within them. Again, we can use pattern matching to achieve this.
+
+Codata, in contrast, is defined in terms of operations we can perform on the elements of the type. These operations are sometimes called *destructors* (which we've already encountered), *observations*, or *eliminators*. A common example of codata is a data structure such as a set. We might define the operations on a `Set` with elements of type `A` as:
+
+- `contains` which takes a `Set[A]` and an element `A` and returns a `Boolean` indicating if the set contains the element;
+- `insert` which takes a `Set[A]` and an element `A` and returns a `Set[A]` containing all the elements from the original set and the new element; and
+- `union` which takes a `Set[A]` and a set `Set[A]` and returns a `Set[A]` containing all the elements of both sets.
+
+In Scala we could implement this definition as
+
+```scala mdoc:silent
+trait Set[A] {
+  
+  /** True if this set contains the given element */
+  def contains(elt: A): Boolean
+  
+  /** Construct a new set containing all elements in this set and the given element */
+  def insert(elt: A): Set[A]
+  
+  /** Construct the union of this and that set */
+  def union(that: Set[A]): Set[A]
+}
+```
+
+This definition does not tell us anything about the internal representation of the elements in the set. It could use a hash table, a tree, or something more exotic. It does, however, tell us what we can do with the set. We know we can take the union but not the intersection, for example. 
+
+If you come from the object-oriented world you might recognize the description of codata above as programming to an interface. In some ways codata is just taking concepts from the object-oriented world and presenting them in a way that is consistent with the rest of the functional programming paradigm. However, this does not mean adopting all the features of object-oriented programming. We won't use state, which is difficult to reason about. We also won't use implementation inheritance either, for the same reason. In our subset of object-oriented programming we'll either be defining interfaces (which may have default implementations of some methods) or final classes that implement those interfaces. Interestingly, this subset of object-oriented programming is often recommended by advocates of object-oriented programming.
+
+Let's now be a little more precise in our definition of codata, which will make the duality between data and codata clearer. Remember the definition of data: it is defined in terms of sums (logical ors) and products (logical ands). We can transform any data into a sum of products. Each product in the sum is a constructor, and the product itself is the parameters that the constructor accepts. Finally, we can think of constructors as functions which take some arbitrary input and produce an element of data. Our end point is a sum of functions from arbitrary input to data.
+
+More abstractly, if we are constructing an element of some data type `A` we call one of the constructors
+
+- `A1: (B, C, ...) => A`; or
+- `A2: (D, E, ...) => A`; or
+- `A3: (F, G, ...) => A`; and so on.
+
+
+Now we'll turn to codata. Codata is defined as a product of functions, these functions being the destructors. The input to a destructor is always an element of the codata type and possibly some other parameters. The output is usually something that is not of the codata type. Thus constructing an element of some codata type `A` means defining
+
+
+- `A1: (A, B, ...) => C`; and
+- `A2: (A, D, ...) => E`; and
+- `A3: (A, F, ...) => G`; and so on.
+
+This hopefully makes the duality between the two clearer.
+
+Now we understand what codata is, we will turn to representing codata in Scala.